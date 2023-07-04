821d0d3bd3e7f005ce918bbb827790fcd7dc1ba5
polloksa <138559664+polloksa@users.noreply.github.com>
2023-07-04 08:37:18 +0200

    Update chat.ts

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -21,6 +21,8 @@ export const modelOptions: ModelOptions[] = [
   'gpt-3.5-turbo',
   'gpt-3.5-turbo-16k',
   'gpt-4',
+  'gpt-4-0314',
+  'gpt-4-0613',
   'gpt-4-32k',
   // 'gpt-3.5-turbo-0301',
   // 'gpt-4-0314',
@@ -90,7 +92,7 @@ export const modelCost = {
   },
 };
 
-export const defaultUserMaxToken = 4000;
+export const defaultUserMaxToken = 8192;
 
 export const _defaultChatConfig: ConfigInterface = {
   model: defaultModel,
